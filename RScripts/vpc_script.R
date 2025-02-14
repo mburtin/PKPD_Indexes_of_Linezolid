ec94aede4a49f35d67afcdd787f3bdeb99d77f28
Michael BURTIN <michael.burtin@univ-poitiers.fr>
2025-02-14 10:41:30 +0100

    RScripts: simply vpc data generation

--- RScripts/vpc_script.R
+++ RScripts/vpc_script.R
@@ -12,8 +12,8 @@ generate_obs_data <- function(data) {
   return(x)
 }
 
-generate_pred_data <- function(data, obs_data, n_sim) {
-  x <- data |> 
+generate_pred_data <- function(sim_data, obs_data, pkpd_index, n_sim) {
+  x <- sim_data |> 
     mutate(PKPD_Index = case_when(
       PKPD_Index %in% c("CENTRAL_Cmax", "CSF_Cmax") ~ "I1_Cmax",
       PKPD_Index %in% c("CENTRAL_AUC_MIC", "CSF_AUC_MIC") ~ "I2_AUC",
@@ -23,9 +23,10 @@ generate_pred_data <- function(data, obs_data, n_sim) {
       TRUE ~ PKPD_Index
     )) |>
     select(-c(data, nls, I0_RSE, Imax_RSE, IC50_RSE, H_RSE, Rsq, Adj.Rsq, Target_stasis, Target_1LogKill, Target_2LogKill)) |>
+    filter(PKPD_Index == pkpd_index) |>
     ungroup()
   
-   x <- x |> left_join(obs_data, by = c("PKPD_Index")) |>
+   x <- x |> left_join(obs_data |> filter(PKPD_Index == pkpd_index), by = c("PKPD_Index")) |>
      slice(rep(1:n(), each = n_sim)) |>
      mutate(REP = rep(1:n_sim, length.out = n()),
             PRED  = Imax_model(value, I0, Imax, IC50, H),
