5ba182de0df91468d773dfaff7a351dea5638552
Michael BURTIN <michael.burtin@univ-poitiers.fr>
2025-05-19 16:48:32 +0200

    VPCs: Major rewrite of the script to use multithreading and reduce memory usage

--- RScripts/vpc_script.R
+++ RScripts/vpc_script.R
@@ -1,20 +1,55 @@
+# Record execution time
+library(tictoc)
+tic()
+
+require(furrr)
+
 #######
 ##  Generic functions
 ######
-generate_obs_data <- function(data) {
+generate_obs_data <- function(data, pkpd_index, n_bins = 20) {
   x <- data |>
     ungroup() |>
     filter(DoseGroup != "control") |>
-    select(STRN, DoseGroup, AMT, ID, PKPD_Index, value, deltaLog10CFU)
+    select(STRN, DoseGroup, AMT, ID, PKPD_Index, value, deltaLog10CFU) |>
+    filter(PKPD_Index == pkpd_index)
 
   if (any(is.na(x))) {
     stop("NA values in obs_data")
   }
 
+  # Use cut for ToverMIC, ntile for others
+  if (pkpd_index == "I3_ToverMIC") {
+    x <- x |>
+      mutate(bin = cut(value, breaks = seq(0, 100, by = 20), right = FALSE))
+  } else {
+    x <- x |>
+      mutate(bin = ntile(value, n_bins))
+  }
+
+  x <- x |>
+    group_by(bin) |>
+    summarize(
+      x_mean = mean(value),
+      p5 = quantile(deltaLog10CFU, 0.05),
+      p50 = quantile(deltaLog10CFU, 0.5),
+      p95 = quantile(deltaLog10CFU, 0.95)
+    )
+
+  message(paste0(" - Empirical data completed for ", pkpd_index))
+
   return(x)
 }
 
-generate_pred_data <- function(sim_data, obs_data, pkpd_index, n_sim) {
+generate_pred_data <- function(sim_data, obs_data, pkpd_index, n_bins = 20) {
+  # Prepare observed data
+  obs_data <- obs_data |>
+    ungroup() |>
+    filter(DoseGroup != "control") |>
+    select(STRN, DoseGroup, AMT, ID, PKPD_Index, value, deltaLog10CFU) |>
+    filter(PKPD_Index == pkpd_index)
+
+  # Prepare simulated data
   x <- sim_data |>
     mutate(PKPD_Index = case_when(
       PKPD_Index %in% c("CENTRAL_Cmax", "CSF_Cmax") ~ "I1_Cmax",
@@ -27,45 +62,49 @@ generate_pred_data <- function(sim_data, obs_data, pkpd_index, n_sim) {
     ungroup()
 
   x <- x |>
-    left_join(obs_data |> filter(PKPD_Index == pkpd_index), by = c("PKPD_Index")) |>
-    slice(rep(1:n(), each = n_sim)) |>
+    left_join(obs_data, by = c("PKPD_Index")) |>
     mutate(
-      REP = rep(1:n_sim, length.out = n()),
       PRED = Emax_model(value, I0, Imax, IC50, H),
       IPRED = Emax_model(value, I0, Imax, IC50, H) + rnorm(n(), mean = 0, sd = Sd_Residuals)
-    ) |>
-    select(STRN, DoseGroup, AMT, ID, PKPD_Index, REP, value, deltaLog10CFU, PRED, IPRED)
+    )
 
   if (any(is.na(x))) {
     stop("NA values in sim_data")
   }
 
+  # Use cut for ToverMIC, ntile for others
+  if (pkpd_index == "I3_ToverMIC") {
+    x <- x |>
+      mutate(bin = cut(value, breaks = seq(0, 100, by = 20), right = FALSE))
+  } else {
+    x <- x |>
+      mutate(bin = ntile(value, n_bins))
+  }
+
+  x <- x |>
+    group_by(bin) |>
+    summarize(
+      x_mean = mean(value),
+      p5 = quantile(IPRED, 0.05),
+      p50 = quantile(IPRED, 0.5),
+      p95 = quantile(IPRED, 0.95)
+    )
+
+
+
   return(x)
 }
 
-ntile_obs_bins <- function(data, n_bins) {
-  data |>
-    mutate(bin = ntile(value, n_bins)) |>
-    group_by(bin) |>
-    summarize(
-      x_mean = mean(value),
-      p5 = quantile(deltaLog10CFU, 0.05),
-      p50 = quantile(deltaLog10CFU, 0.5),
-      p95 = quantile(deltaLog10CFU, 0.95)
-    )
-}
+generate_pred_data_replicates <- function(sim_data, obs_data, pkpd_index, n_replicates = 500, n_bins = 20) {
+  # Generate all replicates
+  replicates <- map(1:n_replicates, ~ {
+    generate_pred_data(sim_data, obs_data, pkpd_index, n_bins) |>
+      mutate(REP = .x)
+  }) |>
+    bind_rows()
 
-ntile_sim_bins <- function(data, n_bins) {
-  data |>
-    group_by(REP) |>
-    mutate(bin = ntile(value, n_bins)) |>
-    group_by(REP, bin) |>
-    summarize(
-      x_mean = mean(value),
-      p5 = quantile(IPRED, 0.05),
-      p50 = quantile(IPRED, 0.5),
-      p95 = quantile(IPRED, 0.95)
-    ) |>
+  # Calculate percentiles between each percentiles of all bins/replicates
+  result <- replicates |>
     pivot_longer(cols = c(p5, p50, p95), names_to = "percentile", values_to = "value") |>
     group_by(percentile, bin, x_mean) |>
     summarize(
@@ -73,85 +112,61 @@ ntile_sim_bins <- function(data, n_bins) {
       ic_50 = quantile(value, 0.5),
       ic_95 = quantile(value, 0.95)
     )
-}
 
-cut_obs_bins <- function(data, seq_start, seq_end, seq_steps) {
-  data |>
-    mutate(bin = cut(value, breaks = seq(seq_start, seq_end, by = seq_steps), right = FALSE)) |>
-    group_by(bin) |>
-    summarize(
-      x_mean = mean(value),
-      p5 = quantile(deltaLog10CFU, 0.05),
-      p50 = quantile(deltaLog10CFU, 0.5),
-      p95 = quantile(deltaLog10CFU, 0.95)
-    )
-}
+  message(paste0(" - Predictions data completed for ", pkpd_index))
 
-cut_sim_bins <- function(data, seq_start, seq_end, seq_steps) {
-  data |>
-    group_by(REP) |>
-    mutate(bin = cut(value, breaks = seq(seq_start, seq_end, by = seq_steps), right = FALSE)) |>
-    group_by(REP, bin) |>
-    summarize(
-      x_mean = mean(value),
-      p5 = quantile(IPRED, 0.05),
-      p50 = quantile(IPRED, 0.5),
-      p95 = quantile(IPRED, 0.95)
-    ) |>
-    pivot_longer(cols = c(p5, p50, p95), names_to = "percentile", values_to = "value") |>
-    group_by(percentile, bin, x_mean) |>
-    summarize(
-      ic_05 = quantile(value, 0.05),
-      ic_50 = quantile(value, 0.5),
-      ic_95 = quantile(value, 0.95)
-    )
+  return(result)
 }
 
 #######
 ##  Dataset generation
 ######
-csf_vpc_data <- list(
-  obs_data = generate_obs_data(csf_data[["sim_data"]]),
-  cmax_sim = generate_pred_data(
-    sim_data = csf_data[["correlation_data"]],
-    obs_data = generate_obs_data(csf_data[["sim_data"]]),
-    pkpd_index = "I1_Cmax",
-    n_sim = 100
-  ),
-  auc_sim = generate_pred_data(
-    sim_data = csf_data[["correlation_data"]],
-    obs_data = generate_obs_data(csf_data[["sim_data"]]),
-    pkpd_index = "I2_AUC",
-    n_sim = 100
-  ),
-  tmic_sim = generate_pred_data(
-    sim_data = csf_data[["correlation_data"]],
-    obs_data = generate_obs_data(csf_data[["sim_data"]]),
-    pkpd_index = "I3_ToverMIC",
-    n_sim = 100
-  )
+message("Starting VPC data generation...")
+
+csf_obs_vpc_data <- list(
+  cmax = generate_obs_data(csf_data[["sim_data"]], pkpd_index = "I1_Cmax"),
+  auc = generate_obs_data(csf_data[["sim_data"]], pkpd_index = "I2_AUC"),
+  tmic = generate_obs_data(csf_data[["sim_data"]], pkpd_index = "I3_ToverMIC")
 )
 
-csf_vpc_with_plasma_index_data <- list(
-  obs_data = generate_obs_data(csf_with_plasma_index_data[["sim_data"]]),
-  cmax_sim = generate_pred_data(
-    sim_data = csf_with_plasma_index_data[["correlation_data"]],
-    obs_data = generate_obs_data(csf_with_plasma_index_data[["sim_data"]]),
-    pkpd_index = "I1_Cmax",
-    n_sim = 100
-  ),
-  auc_sim = generate_pred_data(
-    sim_data = csf_with_plasma_index_data[["correlation_data"]],
-    obs_data = generate_obs_data(csf_with_plasma_index_data[["sim_data"]]),
-    pkpd_index = "I2_AUC",
-    n_sim = 100
-  ),
-  tmic_sim = generate_pred_data(
-    sim_data = csf_with_plasma_index_data[["correlation_data"]],
-    obs_data = generate_obs_data(csf_with_plasma_index_data[["sim_data"]]),
-    pkpd_index = "I3_ToverMIC",
-    n_sim = 100
-  )
+plasma_obs_vpc_data <- list(
+  cmax = generate_obs_data(csf_with_plasma_index_data[["sim_data"]], pkpd_index = "I1_Cmax"),
+  auc = generate_obs_data(csf_with_plasma_index_data[["sim_data"]], pkpd_index = "I2_AUC"),
+  tmic = generate_obs_data(csf_with_plasma_index_data[["sim_data"]], pkpd_index = "I3_ToverMIC")
 )
 
-gc()
+# Setup parallel processing
+plan(multisession)
+
+options(future.globals.maxSize = 2048 * 1024^2)
+
+# Define parameter and start prediction in multithreading
+pkpd_indices <- c("I1_Cmax", "I2_AUC", "I3_ToverMIC")
+names(pkpd_indices) <- c("cmax_sim", "auc_sim", "tmic_sim")
+
+csf_pred_vpc_data <- future_map(pkpd_indices, ~ {
+  generate_pred_data_replicates(
+    sim_data = csf_data[["correlation_data"]],
+    obs_data = csf_data[["sim_data"]],
+    pkpd_index = .x,
+    n_replicates = 500,
+    n_bins = 20
+  )
+}, .options = furrr_options(seed = TRUE))
+
+plasma_pred_vpc_data <- future_map(pkpd_indices, ~ {
+  generate_pred_data_replicates(
+    sim_data = csf_with_plasma_index_data[["correlation_data"]],
+    obs_data = csf_with_plasma_index_data[["sim_data"]],
+    pkpd_index = .x,
+    n_replicates = 500,
+    n_bins = 20
+  )
+}, .options = furrr_options(seed = TRUE))
+
+# Stop parallel processing
+plan(sequential)
+
+# Stop recording time execution
+elapsed <- toc(quiet = TRUE)
+message("VPC data generated in: ", round(elapsed$toc - elapsed$tic, 0), " seconds")
